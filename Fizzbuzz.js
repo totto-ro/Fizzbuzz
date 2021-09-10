d84c7690e5db5d0d65198418ec422b4312f300cc
Sofía Esquivel <sofia.esquivel@lcieducation.net>
2021-09-10 15:48:08 -0600

    Practice

---
+++ Fizzbuzz.js
@@ -0,0 +1,22 @@
+function fizzBuzzSequence( limit ){
+    for( let i = 1; i <= limit; i ++ ){
+        if( i % 5 === 0 && i % 3 === 0 ){
+            console.log( "FizzBuzz" );
+        }
+        else{
+            if( i % 5 === 0 ){
+                console.log( "Buzz" );
+            }
+            else{
+                if( i % 3 === 0 ){
+                    console.log( "Fizz" );
+                }
+                else{
+                    console.log( i );
+                }
+            }
+        }
+    }
+}
+
+fizzBuzzSequence( 100 );
